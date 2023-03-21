d4bae2f9c7a93e778eba1ef00e193eb626431874
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-21 15:46:05 +0100

    Create getPhone() & improve getAllPhones() code style

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,26 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Phone;
 use App\Repository\PhoneRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class PhoneController extends AbstractController
 {
-    #[Route('/api/phones', name: 'app_phone')]
+    #[Route('/api/phones', name: 'app_phones', methods: ['GET'])]
     public function getAllPhones(PhoneRepository $phoneRepository, SerializerInterface $serializer): JsonResponse
     {
         $phoneList = $phoneRepository->findAll();
         $jsonPhoneList = $serializer->serialize($phoneList, 'json');
 
-        return new JsonResponse([
-            $jsonPhoneList,
-            Response::HTTP_OK,
-            [],
-            true
-        ]);
+        return new JsonResponse($jsonPhoneList, Response::HTTP_OK, [], true);
     }
+
+    #[Route('/api/phones/{id}', name: 'app_phone_details', methods: ['GET'])]
+    public function getPhone(Phone $phone, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonPhone = $serializer->serialize($phone, 'json');
+
+        return new JsonResponse($jsonPhone, Response::HTTP_OK, [], true);
+    }
+
 }
