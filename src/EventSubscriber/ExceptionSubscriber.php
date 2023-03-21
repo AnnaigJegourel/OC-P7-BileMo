101af81d6be64cbdecc315c657de4d8cc22c9257
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-21 16:15:16 +0100

    Create ExceptionSubscriber

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof HttpException) {
+            $status = $exception->getStatusCode();
+        } else {
+            $status = 500;
+        }
+        $data = [
+            'status' => $status,
+            'message' => $exception->getMessage()
+        ];
+        $event->setResponse(new JsonResponse($data));
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
