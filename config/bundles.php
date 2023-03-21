7beeb59318a77537f972371de577f97cae799211
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-21 11:26:14 +0100

    Install Doctrine ORM

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
