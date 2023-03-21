6c5a16ba8be3afdefb4f978b7871d528d627e88d
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-21 11:25:16 +0100

    Install Maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-        Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-       ];
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+];
