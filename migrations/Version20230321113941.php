90c46168b31ba0344a2103e82eddca4d0c804440
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-21 14:46:41 +0100

    Codacy: Fix code style

--- migrations/Version20230321113941.php
+++ migrations/Version20230321113941.php
@@ -22,9 +22,10 @@ final class Version20230321113941 extends AbstractMigration
     public function getDescription(): string
     {
         return '';
-        //end getDescription()
+        // end getDescription()
 
     }
+
 
     /**
      * Updates database
@@ -33,8 +34,9 @@ final class Version20230321113941 extends AbstractMigration
      */
     public function up(Schema $schema): void
     {
-        // This up() migration is auto-generated, please modify it to your needs
+        // This up() migration is auto-generated, please modify it to your needs.
         $this->addSql('ALTER TABLE phone CHANGE length width DOUBLE PRECISION DEFAULT NULL');
+
     }
 
     /**
@@ -44,7 +46,7 @@ final class Version20230321113941 extends AbstractMigration
      */
     public function down(Schema $schema): void
     {
-        // This down() migration is auto-generated, please modify it to your needs
+        // This down() migration is auto-generated, please modify it to your needs.
         $this->addSql('ALTER TABLE phone CHANGE width length DOUBLE PRECISION DEFAULT NULL');
     }
 }
