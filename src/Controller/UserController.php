7c46c5953d423187a7f4ee27426a80685d82174b
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-22 12:40:59 +0100

    Create UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class UserController extends AbstractController
+{
+    #[Route('/api/users', name: 'app_user', methods: ['GET'])]
+    public function getAllUsers(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $usersList = $userRepository->findAll();
+        $jsonUsersList = $serializer->serialize($usersList, 'json');
+
+        return new JsonResponse($jsonUsersList, Response::HTTP_OK, [], true);
+    }
+}
