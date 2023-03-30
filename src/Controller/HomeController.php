66b147287d90f001b0988b2315c9323dd8130877
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-30 10:20:02 +0200

    Create HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
