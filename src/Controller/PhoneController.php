7fa0ec8b3acf02309d8e409afc2717f5940f47c0
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-30 14:27:31 +0200

    Use findAllWithPagination() in PhoneController::getAllPhones()

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -9,15 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PhoneController extends AbstractController
 {
 
 
     #[Route('/api/phones', name: 'app_phones', methods: ['GET'])]
-    public function getAllPhones(PhoneRepository $phoneRepository, SerializerInterface $serializer): JsonResponse
+    public function getAllPhones(PhoneRepository $phoneRepository, SerializerInterface $serializer, Request $request): JsonResponse
     {
-        $phoneList = $phoneRepository->findAll();
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 3);
+        // @phpstan-ignore-next-line
+        $phoneList = $phoneRepository->findAllWithPagination($page, $limit);
         $jsonPhoneList = $serializer->serialize($phoneList, 'json');
 
         return new JsonResponse($jsonPhoneList, Response::HTTP_OK, [], true);
