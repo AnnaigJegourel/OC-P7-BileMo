5e81f4369a68cb34082fa52e31105b0d63d26dca
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-04-03 10:11:03 +0200

    Install Asset component & Twig bundle: composer require twig asset

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
