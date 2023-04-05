d4bdec658456be3ca2fe09b51ac595b2e4f6c224
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-04-05 11:36:37 +0200

    Add debug bundle (including monolog & web profiler)

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,7 @@ return [
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
