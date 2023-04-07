bbbc4db801cc759318a2fd15ea0880d1c18a8c30
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-04-07 11:31:20 +0200

    Add expire time to cache in PhoneController

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -73,6 +73,7 @@ class PhoneController extends AbstractController
         $phoneList = $cachePool->get($idCache, function (ItemInterface $item) use ($phoneRepository, $page, $limit) {
             echo ("Cet élément n'est pas encore en cache.");
             $item->tag("phonesCache");
+            $item->expiresAfter(60);
 
             return $phoneRepository->findAllWithPagination($page, $limit);
 
