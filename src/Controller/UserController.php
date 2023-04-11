c22226e14cf26415050e287739bc58094240189c
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-04-11 10:44:57 +0200

    Remove useless space

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -127,7 +127,7 @@ class UserController extends AbstractController
 
         $customerUsersList = $cache->get(
             $idCache,
-            function (ItemInterface $item) use ($userRepository, $idCustomer,  $page, $limit) {
+            function (ItemInterface $item) use ($userRepository, $idCustomer, $page, $limit) {
                 echo ("Cet élément n'est pas encore en cache.");
                 $item->tag("usersCache");
                 $item->expiresAfter(60);
